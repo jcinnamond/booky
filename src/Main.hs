f6c65998cc0b2a1ca3a71e226ed9d9183ff96b5b
John Cinnamond <john@cinnamond.me.uk>
2021-09-16 21:38:01 +0100

    Add POST endpoint, use in-memory db, handle missing user
    Copied from https://docs.servant.dev/en/stable/cookbook/using-custom-monad/UsingCustomMonad.html

--- src/Main.hs
+++ src/Main.hs
@@ -4,53 +4,74 @@
 
 module Main where
 
-import Data.Aeson
-import Data.List (lookup)
-import Network.Wai
-import Network.Wai.Handler.Warp
+import Data.Aeson (FromJSON, ToJSON)
+import Network.Wai (Application)
+import Network.Wai.Handler.Warp (run)
 import Relude
 import Servant
 
-type UserAPI1 =
+type UserAPI =
   "users" :> Get '[JSON] [User]
-    :<|> "users" :> Capture "name" String :> Get '[JSON] User
+    :<|> "users" :> Capture "id" String :> Get '[JSON] (Maybe User)
+    :<|> "users" :> ReqBody '[JSON] User :> PostCreated '[JSON] User
 
 data User = User
   { name :: String
   , age :: Int
   , email :: String
+  , identifier :: String
   }
   deriving (Eq, Show, Generic)
 
 instance ToJSON User
+instance FromJSON User
+
+newtype DB = DB
+  { users :: TVar [User]
+  }
+
+type AppM = ReaderT DB Handler
 
 albert :: User
-albert = User "Albert Einstein" 136 "ae@mc2.org"
+albert = User "Albert Einstein" 136 "ae@mc2.org" "1"
 
 isaac :: User
-isaac = User "Isaac Newton" 372 "isaac@newton.co.uk"
+isaac = User "Isaac Newton" 372 "isaac@newton.co.uk" "2"
 
 users1 :: [User]
 users1 = [albert, isaac]
 
-singleUser :: String -> Handler User
-singleUser u = return $ fromMaybe albert (find (\x -> name x == u) users1)
+getUsers :: AppM [User]
+getUsers = do
+  DB{users = p} <- ask
+  liftIO $ readTVarIO p
 
-server1 :: Server UserAPI1
-server1 =
-  return users1
-    :<|> singleUser
+singleUser :: String -> AppM (Maybe User)
+singleUser i = do
+  DB{users = p} <- ask
+  us <- readTVarIO p
+  return $ find (\x -> identifier x == i) us
 
-userAPI :: Proxy UserAPI1
+createUser :: User -> AppM User
+createUser u = do
+  DB{users = p} <- ask
+  liftIO $ atomically $ readTVar p >>= writeTVar p . (u :)
+  return u
+
+server :: ServerT UserAPI AppM
+server = getUsers :<|> singleUser :<|> createUser
+
+userAPI :: Proxy UserAPI
 userAPI = Proxy
 
--- 'serve' comes from servant and hands you a WAI Application,
--- which you can think of as an "abstract" web application,
--- not yet a webserver.
-app1 :: Application
-app1 = serve userAPI server1
+nt :: DB -> AppM a -> Handler a
+nt s x = runReaderT x s
+
+app :: DB -> Application
+app s = serve userAPI $ hoistServer userAPI (nt s) server
 
 main :: IO ()
 main = do
   putStrLn "running..."
-  run 8081 app1
+  initialUsers <- newTVarIO users1
+  run 8081 $ app $ DB initialUsers
